0df8aa95fa7e1247198e4c2f1aa005eb114117c0
kuroyamii <gerysastrawan123@gmail.com>
2021-12-01 13:54:47 +0800

    Add menu to window

--- App.c
+++ App.c
@@ -8,6 +8,8 @@
 
 /*Window Procedure*/
 LRESULT CALLBACK WindowProcedure(HWND, UINT, WPARAM, LPARAM);
+/*Function to add menu*/
+void addMenu(HWND hwnd);
 /*Win API driver*/
 int WINAPI WinMain(HINSTANCE HInstance, HINSTANCE HPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
@@ -69,6 +71,9 @@ LRESULT CALLBACK WindowProcedure(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lpar
 {
     switch (msg)
     {
+    case WM_CREATE:
+        addMenu(hwnd);
+        break;
     case WM_DESTROY:
         PostQuitMessage(0);
         break;
@@ -76,4 +81,31 @@ LRESULT CALLBACK WindowProcedure(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lpar
         return DefWindowProc(hwnd, msg, wparam, lparam);
     }
     return 0;
+}
+void addMenu(HWND hwnd)
+{
+    /*Declare Menu Handler*/
+
+    HMENU hMenu;
+    HMENU hSubMenuFile;
+    /*Create Menu*/
+
+    hMenu = CreateMenu();
+    hSubMenuFile = CreateMenu();
+
+    /*Append Menu
+    Syntax:
+    AppendMenu(Handler, Menu Type, Pointer or ID, name[str] ) 
+    */
+
+    AppendMenu(hSubMenuFile, MF_STRING, 1, "New");
+    AppendMenu(hSubMenuFile, MF_STRING, 2, "Open");
+    AppendMenu(hSubMenuFile, MF_STRING, 3, "Save");
+    AppendMenu(hSubMenuFile, MF_STRING, 4, "Save As");
+
+    AppendMenu(hMenu, MF_POPUP, (UINT_PTR)hSubMenuFile, "File");
+    AppendMenu(hMenu, MF_STRING, 2, "Edit");
+
+    /*Set the menu to window handler*/
+    SetMenu(hwnd, hMenu);
 }
